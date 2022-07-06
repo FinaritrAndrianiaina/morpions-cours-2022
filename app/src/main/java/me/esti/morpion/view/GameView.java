d544d89e68e0e6968a18a2dee282ca5ae6cb44fa
Finaritra Haritiana Andrianiaina <finaritrandrianiaina@gmail.com>
2022-07-06 16:45:00 +0300

    Dynamiser morpions case

--- app/src/main/java/me/esti/morpion/view/GameView.java
+++ app/src/main/java/me/esti/morpion/view/GameView.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -15,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameView extends View {
-    int gameW = 3;
+    int gameW = 5;
     String CLASS_NAME = this.getClass().getName();
     int[][] matrice = new int[gameW][gameW];
     Paint paint = new Paint();
@@ -34,14 +36,18 @@ public class GameView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        for (int i=0; i<matrice.length;i++) {
+        for (int i = 0; i < matrice.length; i++) {
             int[] row = matrice[i];
-            for (int j=0; j<row.length;j++) {
+            for (int j = 0; j < row.length; j++) {
+                Point size = new Point();
+                getDisplay().getSize(size);
+                int width = size.x / gameW;
+
                 int item = row[j];
-                Log.d(CLASS_NAME,String.format("Matrice[%o][%o]: %o",i,j,item ));
-                int posX = (j) * 301;
-                int posY = (i+1) * 301;
-                Rect rect = new Rect(posX, posY, posX + 300, posY+300);
+                Log.d(CLASS_NAME, String.format("Matrice[%o][%o]: %o", i, j, item));
+                int posX = (j) * width;
+                int posY = (i + 1) * width;
+                Rect rect = new Rect(posX, posY, posX + width, posY + width);
                 canvas.drawRect(rect, paint);
             }
         }
